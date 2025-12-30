71dcd22f22b525dd56f501ac0ea19732c5990e77
Talha Mohammed Khan <tallhakhan5432@gmail.com>
2025-12-30 09:35:03 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'https://dd-backend.cp.hupcfl.com/api',
+        target: 'https://dd-backend.cp.hupcfl.com',
         changeOrigin: true,
       }
     }
